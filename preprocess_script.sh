400fe118df27e1e765a2a73acf5c8129e09de882
Basyrov Rustam <hrustbas@gmail.com>
2025-05-21 20:21:34 +0300

    rename header in preprocessed file

--- preprocess_script.sh
+++ preprocess_script.sh
@@ -2,7 +2,7 @@
 
 # set -xe
 
-echo -e "#CHROM\tPOS\tID\tREF\tALT"
+echo -e "#CHROM\tPOS\tID\tALLELE1\tALLELE2"
 awk '{if (NR>1 && $2 != 23) \
     printf "chr%d\t%d\trs%d\t%s\t%s\n",$2,$4,$1,$5,$6}'\
     ./FP_SNPs.txt
