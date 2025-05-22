5e1a865a97a614b8156602dd60f7f769b5bbc40f
Basyrov Rustam <hrustbas@gmail.com>
2025-05-22 13:40:55 +0300

    parse first argument to preprocess script

--- preprocess_script.sh
+++ preprocess_script.sh
@@ -2,7 +2,14 @@
 
 # set -xe
 
+if [ -n "$1" ]; then
+    INPUT_FILE=$1
+else
+    echo "Need to provide txt-file!" 1>&2
+    exit 1
+fi
+
 echo -e "#CHROM\tPOS\tID\tALLELE1\tALLELE2"
 awk '{if (NR>1 && $2 != 23) \
     printf "chr%d\t%d\trs%d\t%s\t%s\n",$2,$4,$1,$5,$6}'\
-    ./FP_SNPs.txt
+    $INPUT_FILE
