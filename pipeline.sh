e8294a5473c2dbf2f9580f265d92cbfecc5e635e
Basyrov Rustam <hrustbas@gmail.com>
2025-05-22 17:23:20 +0300

    parse arguments in pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/bash
 
+USAGE_MSG="""\
+Usage: $0 -e [ENTRYPOINT] -d [DOCKER IMAGE NAME]\
+"""
+
 DOCKERNAME=biodocker
+
+while getopts "hd:e:" OPT; do
+    case $OPT in
+	d)
+            DOCKERNAME="$OPTARG"
+	    ;;
+        h)
+            echo "$USAGE_MSG"
+            exit 0
+            ;;
+        e)
+            ENTRYPOINT="$OPTARG"
+            echo $ENTRYPOINT
+            ;;
+	\?)
+	    echo "Invalid option: $OPT"
+	    echo "$USAGE_MSG"
+	    exit 1
+    esac
+done
 
 docker build -t $DOCKERNAME .
 
-if [ -n "$1" ]; then
+if [ -n "$ENTRYPOINT" ]; then
     docker run --rm -it \
            -v ${PWD}/data/:/ref/GRCh38.d1.vd1_mainChr/sepChrs/ \
-           $DOCKERNAME "$1"
+           $DOCKERNAME "$ENTRYPOINT"
 else
     docker run --rm -it \
            -v ${PWD}/data/:/ref/GRCh38.d1.vd1_mainChr/sepChrs/ \
